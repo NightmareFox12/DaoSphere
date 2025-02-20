649584293ccd043888c45019bdab5d7dd57734c2
nightmarefox12 <carloshenriquez392004@gmail.com>
2025-02-19 21:04:58 -0400

    Update Next.js configuration with build and webpack optimizations

--- packages/nextjs/next.config.mjs
+++ packages/nextjs/next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-import webpack from "webpack";
+import webpack from 'webpack';
 
 const nextConfig = {
   reactStrictMode: true,
@@ -9,30 +9,30 @@ const nextConfig = {
     remotePatterns: [
       // we might need to add more links. this is just from the starknet ID identicon
       {
-        protocol: "https",
-        hostname: "identicon.starknet.id",
-        pathname: "/**", // Allows all paths under this domain
+        protocol: 'https',
+        hostname: 'identicon.starknet.id',
+        pathname: '/**', // Allows all paths under this domain
       },
       {
-        protocol: "https",
-        hostname: "img.starkurabu.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'img.starkurabu.com',
+        pathname: '/**',
       },
     ],
   },
   typescript: {
-    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreDuringBuilds: true,
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     config.plugins.push(
       new webpack.NormalModuleReplacementPlugin(/^node:(.*)$/, (resource) => {
-        resource.request = resource.request.replace(/^node:/, "");
-      }),
+        resource.request = resource.request.replace(/^node:/, '');
+      })
     );
     return config;
   },
